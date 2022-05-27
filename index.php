78b1f8138ccf39af7d513ad96c4af80b07af442a
johana-cavi <jcajilema73@gmail.com>
2022-05-26 20:42:45 -0500

    Primer cambio de prueba

--- index.php
+++ index.php
@@ -87,6 +87,7 @@ if (!empty($_SESSION['active'])) {
     </div>
     <!-- container-scroller -->
     <!-- plugins:js -->
+    <!-- cambio de ejemplo -->
     <script src="assets/js/material-dashboard.js"></script>
     <!-- endinject -->
 </body>
